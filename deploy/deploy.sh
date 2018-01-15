b6cb4c7e6ede5a60f527bcf52a799144a71134a4
Xiaming Chen <chenxm35@gmail.com>
2018-01-15 16:54:08 +0800

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -31,7 +31,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy/deploy_key.enc -out deploy/deploy_key -d
-chmod 600 $THISPATH/deploy_key
+chmod 600 deploy/deploy_key
 eval `ssh-agent -s`
 ssh-add deploy/deploy_key
 
