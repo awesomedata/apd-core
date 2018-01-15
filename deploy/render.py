f913ac32b996f9ac5498bb3cdb6a57168ea09a3d
Xiaming Chen <chen_xm@outlook.com>
2018-01-16 00:34:45 +0800

    Add link validation

--- deploy/render.py
+++ deploy/render.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
+import logging
 import os
+import sys
+import time
 from collections import OrderedDict
 
+import requests
 import yaml
 from mako.template import Template
+from requests.adapters import HTTPAdapter
+
+logger = logging.getLogger(__name__)
+
+session = requests.Session()
+session.mount('http://', HTTPAdapter(max_retries=3))
+session.mount('https://', HTTPAdapter(max_retries=3))
 
 
-def scan_core_data(core_dir):
+def scan_core_data(core_dir, validate_link=False):
     """Scan and load data entries
     """
     categories = OrderedDict()  # {catetory: [yaml]}
@@ -19,13 +30,41 @@ def scan_core_data(core_dir):
 
         for data_item in sorted(os.listdir(os.path.join(core_dir, category))):
             data_file = os.path.join(core_dir, category, data_item)
+
             try:
                 data_obj = yaml.load(open(data_file))
             except Exception as e:
                 raise RuntimeError('Failed to read YAML data: {}'.format(e))
+
+            if validate_link:
+                homepage = data_obj.get('homepage')
+                data_obj['_status'] = do_validate_link(homepage)
+
             categories[category].append(data_obj)
 
     return categories
+
+
+def write_msg(msg):
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+
+
+def do_validate_link(link):
+    """Validate the accessibility of web like"""
+    _status = False
+    if link:
+        if link.startswith('http') or link.startswith('https'):
+            try:
+                rsp = session.get(link)
+                _status = False if 400 <= rsp.status_code < 600 else True
+                time.sleep(0.1)  # Be nice to servers
+            except Exception as e:
+                logger.warning('Failed to validate link {}, caused by {}'.format(link, e))
+        else:
+            _status = True  # Omit non-http site currently
+    write_msg('Validating {}...{}\n'.format(link, 'OK' if _status else 'FIXME'))
+    return _status
 
 
 if __name__ == '__main__':
@@ -33,7 +72,7 @@ if __name__ == '__main__':
     template_file = os.path.join(pdir, 'index.mako')
     core_dir = os.path.join(pdir, '..', 'core')
 
-    categories = scan_core_data(core_dir)
+    categories = scan_core_data(core_dir, validate_link=True)
 
     rendered = Template(open(template_file).read()).render(categories=categories)
     with open(os.path.join(pdir, 'index.rst'), 'w') as of:
