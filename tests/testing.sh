75f9a4308fff97a87d7ea127f13363bfb5bf255d
Xiaming Chen <chenxm35@gmail.com>
2018-01-19 16:45:04 +0800

    Add OpenSanctions dataset; Add test module

--- tests/testing.sh
+++ tests/testing.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+THISPATH=`dirname $0`
+
+pip install -r $THISPATH/requirements.txt
+
+python $THISPATH/validate.py
---
+++ tests/validate.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from collections import OrderedDict
+
+import os
+import yaml
+
+
+def scan_core_data(core_dir):
+    """Scan and load data entries
+    """
+    categories = OrderedDict()  # {catetory: [yaml]}
+    category_names = os.listdir(core_dir)
+
+    for category in sorted(category_names):
+        print('Scanned category: %s' % category)
+        if category not in categories:
+            categories[category] = list()
+
+        for data_item in sorted(os.listdir(os.path.join(core_dir, category))):
+            data_file = os.path.join(core_dir, category, data_item)
+            categories[category].append(data_file)
+
+    return categories
+
+
+def validate_classification(category_map):
+    for category, entries in category_map.items():
+        for entry in entries:
+            try:
+                data_obj = yaml.load(open(entry))
+            except Exception as e:
+                raise RuntimeError('Failed to read YAML data: {}'.format(e))
+
+            assert data_obj.get('title', None) is not None
+            assert data_obj.get('homepage', None) is not None
+            assert data_obj.get('category', None) is not None
+
+            assert data_obj.get('category') == category
+
+
+if __name__ == '__main__':
+    pdir = os.path.dirname(__file__)
+    core_dir = os.path.join(pdir, '..', 'core')
+
+    categories = scan_core_data(core_dir)
+    validate_classification(categories)
