8c0a74e2746c006bd1e3502423c0ab9d29a5bee3
xiaming.cxm <chenxm35@gmail.com>
2022-10-11 14:01:08 +0800

    fix workflow deploy logic

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-THISPATH=`dirname $0`
-TARGET_REPO="git@github.com:awesomedata/awesome-public-datasets.git"
-
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-# Run our compile script
-function doCompile {
-    pip install -r $THISPATH/requirements.txt
-    python $THISPATH/render.py
-}
-#doCompile
-
-# Clone the target repo into target_repo
-git clone --depth=50 --branch=master $TARGET_REPO target_repo
-
-# Set useful signature info.
-cd target_repo
-git config user.name "xiaming.chen"
-git config user.email "chenxm35@gmail.com"
-
 # Update generated data file
+cd awesome-public-datasets
 cp ../deploy/index.rst README.rst
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
@@ -33,10 +11,13 @@ if git diff --quiet; then
     exit 0
 fi
 
+git config user.name "xiaming.chen"
+git config user.email "chenxm35@gmail.com"
+
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add README.rst
-git commit -m "Update README sha: ${SHA}"
+git commit -m "Update README sha: $(git rev-parse --verify HEAD)"
 
 # Now that we're all set up, we can push.
 git push origin master
